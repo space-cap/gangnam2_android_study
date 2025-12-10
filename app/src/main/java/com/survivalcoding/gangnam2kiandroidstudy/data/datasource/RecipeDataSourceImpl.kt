318ddda9a6f31b36737d23bbaf19e0f8f0e6dd85
이영현 <hong.git@yahoo.com>
2025-12-10 11:57:29 +0900

    feat: Dispatchers.IO

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/datasource/RecipeDataSourceImpl.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/datasource/RecipeDataSourceImpl.kt
@@ -3,16 +3,18 @@ package com.survivalcoding.gangnam2kiandroidstudy.data.datasource
 import android.content.Context
 import com.survivalcoding.gangnam2kiandroidstudy.R
 import com.survivalcoding.gangnam2kiandroidstudy.data.dto.RecipeRootDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 
 class RecipeDataSourceImpl(
     private val context: Context, // Context를 주입받습니다.
 ) : RecipeDataSource {
-    override suspend fun getRecipes(): RecipeRootDto {
+    override suspend fun getRecipes(): RecipeRootDto = withContext(Dispatchers.IO) {
         // res/raw/recipes.json 파일을 엽니다.
         val inputStream = context.resources.openRawResource(R.raw.recipes)
 
         // kotlinx.serialization.json.Json을 사용하여 스트림에서 직접 디코딩합니다.
-        return Json.decodeFromString(inputStream.bufferedReader().use { it.readText() })
+        return@withContext Json.decodeFromString(inputStream.bufferedReader().use { it.readText() })
     }
 }
