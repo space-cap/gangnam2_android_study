ccb626ff36cd922879857529f2fb984904247ffe
이영현 <hong.git@yahoo.com>
2025-12-10 10:00:02 +0900

    feat: 세로 방향 간격에 width 대신 height를 사용

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saveedrecipes/SavedRecipesScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saveedrecipes/SavedRecipesScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -75,7 +74,7 @@ fun SavedRecipesScreen(
                             rating = recipe.rating
                         )
 
-                        Spacer(Modifier.width(20.dp))
+                        Spacer(Modifier.height(20.dp))
                     }
                 }
             }
