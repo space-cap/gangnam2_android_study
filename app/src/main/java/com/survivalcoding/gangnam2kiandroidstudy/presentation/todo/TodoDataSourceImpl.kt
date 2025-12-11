6b3c68a95510082893f63831d3650f2fee107bce
이영현 <hong.git@yahoo.com>
2025-12-11 10:31:11 +0900

    feat: addTodo 수정

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/todo/TodoDataSourceImpl.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/todo/TodoDataSourceImpl.kt
@@ -1,14 +1,19 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.todo
 
 class TodoDataSourceImpl : TodoDataSource {
+
+    // 간단한 데모용 인메모리 리스트
+    private val todos = mutableListOf(
+        Todo(1, "Todo 1", "Description 1", false),
+        Todo(2, "Todo 2", "Description 2", true),
+    )
+
     override suspend fun getTodos(): List<Todo> {
-        return listOf(
-            Todo(1, "Todo 1", "Description 1", false),
-            Todo(2, "Todo 2", "Description 2", true),
-        )
+        // 외부에서 변경되지 않도록 복사본 반환
+        return todos.toList()
     }
 
     override suspend fun addTodo(todo: Todo) {
-        
+        todos.add(todo)
     }
 }
