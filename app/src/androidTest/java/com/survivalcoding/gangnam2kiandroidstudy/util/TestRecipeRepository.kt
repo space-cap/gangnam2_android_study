69956f9be98313d3c0ec6550bbdfb435144cd58a
이영현 <hong.git@yahoo.com>
2025-12-17 10:55:01 +0900

    feat: TODO 구현

--- app/src/androidTest/java/com/survivalcoding/gangnam2kiandroidstudy/util/TestRecipeRepository.kt
+++ app/src/androidTest/java/com/survivalcoding/gangnam2kiandroidstudy/util/TestRecipeRepository.kt
@@ -10,18 +10,18 @@ class TestRecipeRepository(private val initialRecipes: List<Recipe>) : RecipeRep
     }
 
     override suspend fun getSavedRecipes(): List<Recipe> {
-        TODO("Not yet implemented")
+        return emptyList()  // 또는 테스트용 저장된 레시피 목록 반환
     }
 
     override suspend fun getRecipeById(recipeId: Long): Recipe? {
-        TODO("Not yet implemented")
+        return initialRecipes.find { it.id == recipeId }
     }
 
     override suspend fun isRecipeSaved(recipeId: Long): Boolean {
-        TODO("Not yet implemented")
+        return false  // 또는 테스트 시나리오에 맞는 로직
     }
 
     override suspend fun setRecipe(recipe: Recipe) {
-        TODO("Not yet implemented")
+        // 테스트용 구현: 필요시 내부 리스트 업데이트
     }
 }
